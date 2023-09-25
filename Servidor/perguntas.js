391a055560a805d98ddc6ad783323d76f3931688
WiseFoxes <sesiwisefoxes@gmail.com>
2023-09-25 15:39:52 -0300

    alteramos as alternativas

--- Servidor/perguntas.js
+++ Servidor/perguntas.js
@@ -11,45 +11,45 @@
                 }
             },
             {
-                texto: "Roupas sociais",
+                texto: "Roupas despojadas",
                 pontuacao: {
                     Jazz: 1
                 }
             },
             {
-                texto: "Roupas de diferentes culturas",
+                texto: "Vários estilos",
                 pontuacao: {
-                    Ballet: 1
+                    Contemporânea: 1
                 }
             },
             {
-                texto: "Roupas alegres e despojadas",
+                texto: "Roupas alegres",
                 pontuacao: {
-                    Ballet: 1
+                    Samba: 1
                 }
             },
             {
                 texto: "Roupas justas, longas e fluídas",
                 pontuacao: {
-                    Ballet: 1
+                    DançaDoVentre: 1
                 }
             },
             {
                 texto: "Estampas coloridas",
                 pontuacao: {
-                    Ballet: 1
+                    Forró: 1
                 }
             },
             {
-                texto: "Roupas básicas com acessórios",
+                texto: "Roupas sociais",
                 pontuacao: {
-                    Ballet: 1
+                    Sapateado: 1
                 }
             },
             {
                 texto: "Calças largas, camisetas coloridas",
                 pontuacao: {
-                    Ballet: 1
+                    Breakdance: 1
                 }
             },
     
@@ -63,52 +63,47 @@
             {
                 texto: "Alegre",
                 pontuacao: {
-                    Ballet: 1
+                    Samba: 1,
+                    Sapateado: 1
                 }
             },
             {
                 texto: "Romântico(a)",
                 pontuacao: {
-                    Jazz: 1
+                    Ballet: 1,
+                    Contemporânea: 1,
+                    Forró: 1
                 }
             },
             {
                 texto: "Sensual",
                 pontuacao: {
-                    Ballet: 1
-                }
-            },
-            {
-                texto: "Tradicional",
-                pontuacao: {
-                    Ballet: 1
+                    DançaDoVentre: 1,
+                    Jazz: 1
                 }
             },
             {
                 texto: "Extrovertido(a)",
                 pontuacao: {
-                    Ballet: 1
+                    Breakdance: 1,
+                    Forró: 1, 
+                    Samba: 1
                 }
             },
             {
                 texto: "Sonhador(a)",
                 pontuacao: {
+                    Contemporânea: 1,
                     Ballet: 1
                 }
             },
             {
                 texto: "Delicado(a)",
                 pontuacao: {
-                    Ballet: 1
+                    Ballet: 1,
+                    Contemporânea: 1
                 }
             },
-            {
-                texto: "Caridoso(a)",
-                pontuacao: {
-                    Ballet: 1
-                }
-            },
-    
         ]
     },
     {
@@ -118,43 +113,40 @@
             {
                 texto: "Sapatilha",
                 pontuacao: {
-                    Ballet: 1
+                    Ballet: 1,
+                    Contemporânea: 1, 
+                    Jazz:1
                 }
             },
             {
                 texto: "All Star",
                 pontuacao: {
-                    Jazz: 1
-                }
-            },
-            {
-                texto: "Tênis esportivo",
-                pontuacao: {
-                    Ballet: 1
+                    Breakdance: 1
                 }
             },
             {
                 texto: "Salto Alto",
                 pontuacao: {
-                    Ballet: 1
+                    Samba: 1,
                 }
             },
             {
                 texto: "Chinelo",
                 pontuacao: {
-                    Ballet: 1
+                    DançaDoVentre: 1
                 }
             },
             {
                 texto: "Sándalia da salto baixo",
                 pontuacao: {
-                    Ballet: 1
+                    Forró: 1,
                 }
             },
             {
                 texto: "Social",
                 pontuacao: {
-                    Ballet: 1
+                    Jazz: 1,
+                    Sapateado: 1
                 }
             },
         ]
@@ -166,49 +158,45 @@
             {
                 texto: "Violino",
                 pontuacao: {
-                    Ballet: 1
+                    Ballet: 1, 
+                    Contemporânea: 1
                 }
             },
             {
                 texto: "Bateria",
                 pontuacao: {
-                    Jazz: 1
+                    Breakdance: 1
                 }
             },
             {
                 texto: "Piano",
                 pontuacao: {
+                    Contemporânea: 1, 
                     Ballet: 1
                 }
             },
             {
                 texto: "Pandeiro",
                 pontuacao: {
-                    Ballet: 1
+                    Samba: 1
                 }
             },
             {
                 texto: "Flauta",
                 pontuacao: {
-                    Ballet: 1
+                    DançaDoVentre: 1
                 }
             },
             {
                 texto: "Sanfona",
                 pontuacao: {
-                    Ballet: 1
-                }
-            },
-            {
-                texto: "Chocalho",
-                pontuacao: {
-                    Ballet: 1
+                    Forró: 1
                 }
             },
             {
                 texto: "Saxofone",
                 pontuacao: {
-                    Ballet: 1
+                    Jazz: 1
                 }
             },
         ]
@@ -220,11 +208,12 @@
             {
                 texto: "Clássico",
                 pontuacao: {
-                    Ballet: 1
+                    Ballet: 1, 
+                    Contemporânea: 1
                 }
             },
             {
-                texto: "Folclórica",
+                texto: "Jazz",
                 pontuacao: {
                     Jazz: 1
                 }
@@ -232,37 +221,32 @@
             {
                 texto: "Hip-Hop",
                 pontuacao: {
-                    Ballet: 1
+                    Breakdance: 1
                 }
             },
             {
-                texto: "Maxixe",
+                texto: "Pagode",
                 pontuacao: {
-                    Ballet: 1
+                    Samba: 1
                 }
             },
             {
                 texto: "Pop",
                 pontuacao: {
-                    Ballet: 1
+                    Jazz: 1,
+                    Sapateado: 1
                 }
             },
             {
                 texto: "Embolada",
                 pontuacao: {
-                    Ballet: 1
-                }
-            },
-            {
-                texto: "Latina",
-                pontuacao: {
-                    Ballet: 1
+                    Forró: 1
                 }
             },
             {
                 texto: "Funk",
                 pontuacao: {
-                    Ballet: 1
+                    Breakdance: 1
                 }
             },
         ]
